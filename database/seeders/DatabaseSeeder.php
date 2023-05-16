b0b2ed1821bc8cdfe1121dc8f6868993efa8f829
Adam <zzz.dzudo@gmail.com>
2023-05-16 13:22:59 +0300

    added seedrs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Cart;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\SubCategory;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +19,34 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+         ]);
+
+         for ($i=1;$i<=10;$i++){
+             Category::create([
+                 'title' => fake()->streetName
+             ]);
+
+             Category::create([
+                 'title' => fake()->streetName,
+                 'sub_category_id' => Category::get()->random()->id
+             ]);
+         }
+
+        for($i=1;$i<=10;$i++){
+            Product::create([
+                'name' => fake()->colorName,
+                'price' => fake()->numberBetween(50, 1000),
+                'category_id' => Category::get()->random()->id,
+            ]);
+        }
+        for($i=1;$i<=5;$i++){
+            Cart::create([
+                'product_id' => Product::get()->random()->id,
+                'user_id' => User::first()->id,
+            ]);
+        }
     }
 }
